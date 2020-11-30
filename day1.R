042c55d0f5e9a3485bc7af1eca5b4c26742dba0f
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2020-11-30 02:40:55 -0800

    optim

--- day1.R
+++ day1.R
@@ -28,9 +28,39 @@ RSS = function(par, obs, ...) {
   return(rss)
 }
 
-RSS(par=c(0.4, 120), obs=obs, C=C)
+RSS(par=c(0.31, 101), obs=obs, C=C)
+
+optim(par=c(0.31, 101), fn=RSS, obs=obs, C=C)
+
+optim(par=c(0.8, 500), fn=RSS, obs=obs, C=C)
+
+opt0 = optim(par=c(0.8, 500), fn=RSS, obs=obs, C=C,
+      control=list(maxit=500))
+
+opt1 = optim(par=opt0$par, fn=RSS, obs=obs, C=C,
+      control=list(maxit=500))
+
+opt2 = optim(par=opt1$par, fn=RSS, obs=obs, C=C,
+      control=list(maxit=500))
 
 
+optb0 = optim(par=c(0.8, 500), fn=RSS, obs=obs, C=C,
+             method="BFGS")
+
+optc0 = calibrate(par=c(0.8, 500), fn=RSS, obs=obs, C=C,
+              method="BFGS")
+
+rs = seq(from=0.01, to=0.5, by=0.01)
+Ks = seq(from=50, to=500, by=10)
+
+pars = expand.grid(r=rs, K=Ks)
+
+out = apply(pars, 1, FUN=RSS, obs=obs, C=C)
+dim(out) = c(length(rs), length(Ks))
+
+library(fields)
+image.plot(rs, Ks, log(out+1e-9))
+points(0.3, 100, col="red",pch=1)
 
 
 
