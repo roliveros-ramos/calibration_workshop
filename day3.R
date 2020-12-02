12fda540bb40eb37be921ba3aaddb9d7d5824b48
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2020-12-02 01:07:37 -0800

    last version

--- day3.R
+++ day3.R
@@ -2,17 +2,17 @@
 # simulator of the logistic model
 runModel = function(par, T=100) {
   # translating the 'par' vector to be our model
-  r  = par[1]
-  K  = par[2]
-  h  = par[3]
-  B0 = par[4]
+  r  = par[1] # par["r"]
+  K  = par[2] # par["K"]
+  h  = par[3] # par["h"]
+  B0 = par[4] # par["B0"]
   time = seq_len(T)
   B = rep(NA, T)
   C = rep(NA, T)
   B[1] = B0
   for(t in time[-T]) {
     B[t+1] = B[t] + r*B[t]*(1-B[t]/K)
-    C[t]   = min(exp(h)*B[t], 0.99*B[t+1])
+    C[t]   = min(h*B[t], 0.99*B[t+1])
     B[t+1] = B[t+1] - C[t]
   }
   return(list(biomass=B, catch=C))
@@ -32,6 +32,12 @@ real_par = c(r=0.2, K=1024, h=0.1, B0=301)
 obs = runModel(real_par)
 
 plot(obs$biomass)
+plot(obs$catch)
+
+guess = c(r=0.1, K=800, h=0.2, B0=500)
+
+calibrate(par=guess, fn=errorFunction, obs=obs)
+
 
 
 
